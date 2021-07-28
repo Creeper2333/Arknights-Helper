85e4e66d50a83143bb4392497efbfc768f17ea16
Creeper233 <52315359+Creeper2333@users.noreply.github.com>
2021-07-28 19:12:41 +0800

    Update akhelper.py

--- akhelper.py
+++ akhelper.py
@@ -5,8 +5,11 @@ from PIL import Image
 import sys
 import time
 
+TIME_ELAPSE_SCRSHOT=3#检测间隔时间，太长则很迟钝，太短会频繁写入文件
+TIME_ELAPSE_BATTLE=50#战斗时间，减少无用截屏次数
+
 coordinates={
-    'select_op':(1065,370),
+    'select_op':(1065,370),#在这里填上要选择的关卡的屏幕坐标
     'start_op':(1428,848),
     'op_begin':(1375,644),
     'start_op_btn_1':(1300,830),
@@ -21,10 +24,11 @@ boxes={
     'op_begin_btn':(1295,460,1464,808),
     'sanity':(1010,323,1343,606)
 }
+flag_isinbattle=False
 
 def screenshot():
     scrcap('/sdcard/capture.png')
-    time.sleep(1)
+    time.sleep(1)#防止未截屏完而导出文件导致文件损坏
     pull('/sdcard/capture.png')
 
 def start_btn_cpr(img):
@@ -52,6 +56,7 @@ def dosth():
             return True
     if(begin_btn_cpr(img_origin)<10):
         tap(coordinates['op_begin'])
+        flag_isinbattle=True
         return True
     if(sanity(img_origin)<10):
         input('没有理智了')
@@ -66,10 +71,11 @@ if(__name__=='__main__'):
             print('截屏失败，可能是模拟器未正常启动')
             #sys.exit(0)
         #img=Image.open('ak_ui/op_select.png')
-        time.sleep(1)
+        time.sleep(1)#防止文件没有导出完
         img_origin=Image.open('capture.png')
         if(not dosth()):
             tap(coordinates['select_op'])
-        
+        if(flag_isinbattle):
+            time.sleep(TIME_ELAPSE_BATTLE)
         img_origin.close()
-        time.sleep(3)
+        time.sleep(TIME_ELAPSE_SCRSHOT)
